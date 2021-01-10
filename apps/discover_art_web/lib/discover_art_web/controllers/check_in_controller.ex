2dc52873f357e5e3dec48f59c9ed17fbdb40e888
Peyton Seigo <pseigo@yahoo.com>
2021-01-10 00:52:12 -0800

    Fix some bugs

--- apps/discover_art_web/lib/discover_art_web/controllers/check_in_controller.ex
+++ apps/discover_art_web/lib/discover_art_web/controllers/check_in_controller.ex
@@ -3,24 +3,31 @@ defmodule DiscoverArtWeb.CheckInController do
 
   alias DiscoverArt.Accounts
   alias DiscoverArt.Accounts.CheckIn
+  alias DiscoverArt.Locations
 
-  def create(conn, %{"check_in" => check_in_params}) do
-    with {:ok, %CheckIn{} = check_in} <- Accounts.create_check_in(check_in_params) do
+  def create(conn, %{"user_id" => _user_id, "public_art_id" => public_art_id} = check_in_params) do
+    Locations.maybe_create_public_art(%{id: public_art_id})
+
+    with {:ok, %CheckIn{} = check_in} <- Accounts.maybe_create_check_in(check_in_params) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", Routes.check_in_path(conn, :show, check_in))
       |> render("show.json", check_in: check_in)
     end
+  end
+
+  def show(conn, %{"check_in_id" => check_in_id}) do
+    check_in = Accounts.get_check_in!(check_in_id)
+    render(conn, "show.json", check_in: check_in)
   end
 
   def show(conn, %{"user_id" => user_id}) do
     # check_ins = Accounts.list_check_ins(String.to_integer(user_id))
     check_ins = Accounts.list_check_ins(user_id)
-    render(conn, "show.json", check_ins: check_ins)
+    render(conn, "index.json", check_ins: check_ins)
   end
 
-  def delete(conn, %{"id" => id}) do
-    check_in = Accounts.get_check_in!(id)
+  def delete(conn, %{"user_id" => user_id, "public_art_id" => public_art_id} = check_in_params) do
+    check_in = Accounts.get_check_in!(user_id, public_art_id)
 
     with {:ok, %CheckIn{}} <- Accounts.delete_check_in(check_in) do
       send_resp(conn, :no_content, "")
--- apps/discover_art_web/lib/discover_art_web/router.ex
+++ apps/discover_art_web/lib/discover_art_web/router.ex
@@ -17,8 +17,8 @@ defmodule DiscoverArtWeb.Router do
   scope "/api", DiscoverArtWeb do
     pipe_through :api
 
-    resources "/users", UserController
     get "/map", MapController, :index
+    resources "/users", UserController, only: [:index, :create, :show, :update, :delete]
 
     scope "/check_ins" do
       get    "/:user_id",                CheckInController, :show
