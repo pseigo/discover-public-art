006ecd8a24d0e4eed4ee31bfb7399ef2b95502dc
Jeffrey Chen <jeffreychen246@gmail.com>
2021-01-09 20:10:15 -0800

    Add route for check ins

--- apps/discover_art_web/assets/js/Root.jsx
+++ apps/discover_art_web/assets/js/Root.jsx
@@ -2,13 +2,15 @@ import * as React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './Login';
 import Profile from './Profile';
+import Checkins from './Checkins';
 
 const Root = () => (
   <>
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Login }/>
-        <Route exact path="/" component={ Profile }/>
+        <Route exact path="/Profile" component={ Profile }/>
+        <Route exact path="/Checkins" component={ Checkins }/>
       </Switch>
     </BrowserRouter>
   </>
