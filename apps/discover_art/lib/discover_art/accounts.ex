834181166348099f296a04b9ed4cf38d8a9b5bc6
Peyton Seigo <pseigo@yahoo.com>
2021-01-10 11:42:54 -0800

    Login stuff is almost working, but just sesnding user data for now

--- apps/discover_art/lib/discover_art/accounts.ex
+++ apps/discover_art/lib/discover_art/accounts.ex
@@ -6,7 +6,41 @@ defmodule DiscoverArt.Accounts do
   import Ecto.Query, warn: false
   alias DiscoverArt.Repo
 
-  alias DiscoverArt.Accounts.User
+  alias DiscoverArt.Accounts.{User, UserToken}
+
+    @doc """
+  Gets a user by email.
+
+  ## Examples
+
+      iex> get_user_by_email("foo@example.com")
+      %User{}
+
+      iex> get_user_by_email("unknown@example.com")
+      nil
+
+  """
+  def get_user_by_email(email) when is_binary(email) do
+    Repo.get_by(User, email: email)
+  end
+
+  @doc """
+  Gets a user by email and password.
+
+  ## Examples
+
+      iex> get_user_by_email_and_password("foo@example.com", "correct_password")
+      %User{}
+
+      iex> get_user_by_email_and_password("foo@example.com", "invalid_password")
+      nil
+
+  """
+  def get_user_by_email_and_password(email, password)
+      when is_binary(email) and is_binary(password) do
+    user = Repo.get_by(User, email: email)
+    if User.valid_password?(user, password), do: user
+  end
 
   @doc """
   Returns the list of users.
@@ -100,6 +134,33 @@ defmodule DiscoverArt.Accounts do
   """
   def change_user(%User{} = user, attrs \\ %{}) do
     User.changeset(user, attrs)
+  end
+
+  ## Session
+
+  @doc """
+  Generates a session token.
+  """
+  def generate_user_session_token(user) do
+    {token, user_token} = UserToken.build_session_token(user)
+    Repo.insert!(user_token)
+    token
+  end
+
+  @doc """
+  Gets the user with the given signed token.
+  """
+  def get_user_by_session_token(token) do
+    {:ok, query} = UserToken.verify_session_token_query(token)
+    Repo.one(query)
+  end
+
+  @doc """
+  Deletes the signed token with the given context.
+  """
+  def delete_session_token(token) do
+    Repo.delete_all(UserToken.token_and_context_query(token, "session"))
+    :ok
   end
 
   alias DiscoverArt.Accounts.CheckIn
