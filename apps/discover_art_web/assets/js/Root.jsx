fc18dc156b051a338b51965a949022e2990b1c85
Jeffrey Chen <jeffreychen246@gmail.com>
2021-01-10 00:18:47 -0800

    Add route to map

--- apps/discover_art_web/assets/js/Root.jsx
+++ apps/discover_art_web/assets/js/Root.jsx
@@ -3,7 +3,11 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './Login';
 import Profile from './Profile';
 import Checkins from './Checkins';
+<<<<<<< HEAD
 import Register from './Register';
+=======
+import MapPage from './MapPage';
+>>>>>>> Add route to map
 
 const Root = () => (
   <>
@@ -12,7 +16,11 @@ const Root = () => (
         <Route exact path="/" component={ Login }/>
         <Route exact path="/Profile" component={ Profile }/>
         <Route exact path="/Checkins" component={ Checkins }/>
+<<<<<<< HEAD
         <Route path="/Register" component={ Register }/>
+=======
+        <Route exact path="/Map" component={ MapPage }/>
+>>>>>>> Add route to map
       </Switch>
     </BrowserRouter>
   </>
