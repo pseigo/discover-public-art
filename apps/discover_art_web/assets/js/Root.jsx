cbb866fa0c35f5805eb0a8c443749ec3a115fb39
Aiman Ismail <46500672+aimanismail21@users.noreply.github.com>
2021-01-09 19:10:55 -0800

    Revert "(#10) Login Page"

--- apps/discover_art_web/assets/js/Root.jsx
+++ apps/discover_art_web/assets/js/Root.jsx
@@ -1,15 +1,21 @@
 import * as React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Login from './Login';
 
 const Root = () => (
   <>
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={ Login }/>
+        <Route exact path="/" component={ HelloWorld } />
       </Switch>
     </BrowserRouter>
   </>
+);
+
+const HelloWorld = () => (
+  <div>
+    <h1>Discover Public Art</h1>
+    <p>Welcome to React!</p>
+  </div>
 );
 
 export default Root;
