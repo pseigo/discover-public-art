54dbb5de98d21293cc0505b1f4681324466c8409
jessehu312 <jessehu312@gmail.com>
2021-01-10 11:23:11 -0800

    Login and Register link up

--- apps/discover_art_web/assets/js/Login.jsx
+++ apps/discover_art_web/assets/js/Login.jsx
@@ -2,17 +2,19 @@ import React, { useState } from 'react';
 import { Form } from 'react-bootstrap';
 import "../css/Login.css";
 import Button from './Button';
+import { useUser } from './UserProvider';
 
 const Login = ({history}) => {
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
+  const {login, me} = useUser();
 
   const toRegister = (data) => {
     let pathname = '/Register';
     history.replace({pathname, state: data});
   }
 
-  return (
+  return me ? null : (
     <div className="login-container">
       <div className="instructions">Please sign in to save your progress:</div>
         <Form className="box-container">
@@ -23,7 +25,7 @@ const Login = ({history}) => {
             <Form.Control placeholder=" Password:" value={pass} onChange={(e) => setPass(e.target.value)} />
           </Form.Group>
           <Form.Group className="login-buttons">
-            <Button primary>
+            <Button onClick={ () => login(email, pass)} primary>
               Sign in
             </Button>
             <Button onClick={ () => toRegister({email, pass})} primary>
---
+++ apps/discover_art_web/assets/js/UserProvider.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useContext, createContext } from "react";
+
+const UserContext = createContext({
+  me: null,
+});
+
+export function UserProvider({ children }) {
+  const [me, setMe] = useState(null);
+
+  const login = (email, pass) => {
+      return fetch(
+        '/auth/login',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            'Accept': 'application/json',
+          },
+          body: new URLSearchParams({
+            email,
+            password: pass
+          })
+        }
+      )
+      .then(((resp) => resp.json()))
+      .then(({data}) => {
+        console.log(data);
+        setMe(data)
+        location.href = '/';
+      })
+      .catch((error) => console.log(error));
+  }
+
+  const register = (email, pass, name) => {
+    return fetch(
+      '/api/users',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+          'Accept': 'application/json',
+        },
+        body: new URLSearchParams({
+          email,
+          password: pass,
+          display_name: name
+        })
+      }
+    )
+    .then(((resp) => resp.json()))
+    .then(({data}) => {
+      console.log(data);
+      setMe(data)
+      location.href = '/';
+    })
+    .catch((error) => console.log(error));
+  }
+
+  return (
+    <UserContext.Provider value={{ me, login, register }}>{children}</UserContext.Provider>
+  )
+}
+
+export const useUser = () => {
+  return useContext(UserContext);
+};
