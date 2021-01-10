a0e5b7a6288b1e09dde9a03e442069aaef35501c
Jeffrey Chen <jeffreychen246@gmail.com>
2021-01-10 05:51:08 -0800

    Support page with PageButton components

--- apps/discover_art_web/assets/js/Components.jsx
+++ apps/discover_art_web/assets/js/Components.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 import { Button, Image } from 'react-bootstrap';
 import '../css/components.css';
 
-export const PageButton = ({ className }) => {
+export const PageButton = ({ className, path }) => {
+  const history = useHistory();
+
+  function handleClick() {
+    history.push(`/${path}`);
+  }
+
   return (
-    <Image className={[className, "page-button"].join(' ')} src="../images/dummy-profile.jpeg" roundedCircle />
+    <Image className={[className, "page-button"].join(' ')} src="../images/dummy-profile.jpeg" onClick={() => handleClick()} roundedCircle />
   );
 };
