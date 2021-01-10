ea27f79e53bc243a3fcaf781adde71f01b83d759
Jeffrey Chen <jeffreychen246@gmail.com>
2021-01-09 19:12:37 -0800

    Clean up root file

---
+++ apps/discover_art_web/assets/js/Profile.jsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Button, Image } from 'react-bootstrap';
+import { PageButton } from './Components';
+import '../css/profile.css';
+
+const Profile = () => (
+  <div className="profile">
+    <h1 className="page-title">Profile</h1>
+    <div className="profile__header">
+      <Image className="profile__image" src="../images/dummy-profile.jpeg" roundedCircle />
+      <div className="header__desc">
+        <h3 className="profile__name">Stephanie Kale</h3>
+        <p className="profile__desc">I like exploring new places in my local city! I love travelling too!</p>
+      </div>
+    </div>
+    <div className="profile__stats">
+      <div className="profile__stat stat__checkins">
+        <h3>101</h3>
+        <p>Check Ins</p>
+      </div>
+      <div className="profile__stat stat__friends">
+        <h3>46</h3>
+        <p>Friends</p>
+      </div>
+    </div>
+    <div className="profile__options">
+      <Button className="options__btn">View Check Ins</Button>
+      <Button className="options__btn">Logout</Button>
+    </div>
+    <PageButton className="map-btn" />
+  </div>
+);
+
+
+
+export default Profile;
--- apps/discover_art_web/assets/js/Root.jsx
+++ apps/discover_art_web/assets/js/Root.jsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './Login';
+import Profile from './Profile';
 
 const Root = () => (
   <>
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Login }/>
+        <Route exact path="/" component={ Profile }/>
       </Switch>
     </BrowserRouter>
   </>
